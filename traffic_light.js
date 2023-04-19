4410384a8175190e34bfb94eec2ef9363f27049f
srimaailuri <97750882+srimaailuri@users.noreply.github.com>
2023-04-19 18:16:07 +0530

    Add files via upload

---
+++ traffic_light.js
@@ -0,0 +1,26 @@
+function stop_button() {
+    document.getElementById("stopButton").style.backgroundColor = "#cf1124";
+    document.getElementById("readyButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("goButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("stopLight").style.backgroundColor = "#cf1124";
+    document.getElementById("readyLight").style.backgroundColor = "#4b5069";
+    document.getElementById("goLight").style.backgroundColor = "#4b5069";
+}
+
+function ready_button() {
+    document.getElementById("stopButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("readyButton").style.backgroundColor = "#f7c948";
+    document.getElementById("goButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("stopLight").style.backgroundColor = "#4b5069";
+    document.getElementById("readyLight").style.backgroundColor = "#f7c948";
+    document.getElementById("goLight").style.backgroundColor = "#4b5069";
+}
+
+function go_button() {
+    document.getElementById("stopButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("readyButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("goButton").style.backgroundColor = "#199473";
+    document.getElementById("stopLight").style.backgroundColor = "#4b5069";
+    document.getElementById("readyLight").style.backgroundColor = "#4b5069";
+    document.getElementById("goLight").style.backgroundColor = "#199473";
+}
